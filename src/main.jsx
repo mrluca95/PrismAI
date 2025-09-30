94f3f631c187e6258abd88322e72bd87e0c866e6
Luca Bella <luca.bella1995@gmail.com>
2025-09-30 21:28:14 +0200

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-﻿import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { CurrencyProvider } from '@/context/CurrencyContext.jsx';
 import './index.css';
 
+const basename = (import.meta.env.BASE_URL || '/').replace(/\/$/, '') || '/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename === '/' ? undefined : basename}>
       <CurrencyProvider>
         <App />
       </CurrencyProvider>
